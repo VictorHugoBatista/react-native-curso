3844879742c7d19cb5a4fe00409c16d4fb0c1def
Victor Hugo Batista <vct.bta@gmail.com>
2021-03-05 11:54:30 -0300

    Adiciona navegação por tabs às telas

--- navigation/src/views/TelaB.js
+++ navigation/src/views/TelaB.js
@@ -3,9 +3,11 @@ import React from 'react';
 import TelaCentral from '../components/TextoCentral';
 
 export default props => {
+    const route = props.route;
+    const numero = route && route.params && route.params.numero || 0;
     return (
     <TelaCentral colorBg='#3b82c4'>
-        Tela B (Param {props.route.params.numero})
+        Tela B (Param {numero})
     </TelaCentral>
     );
 };
