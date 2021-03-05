1e179cc3fef38d42aac6cdaa4162c97be44137a8
Victor Hugo Batista <vct.bta@gmail.com>
2021-03-05 16:41:35 -0300

    Adiciona ícones à navegação por tabs

--- navigation/src/nav/Tab.js
+++ navigation/src/nav/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import TelaA from '../views/TelaA';
 import TelaB from '../views/TelaB';
@@ -8,13 +9,31 @@ import TelaC from '../views/TelaC';
 const Tab = createBottomTabNavigator();
 
 export default props => (
-    <Tab.Navigator tabBarOptions={{
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          switch (route.name) {
+            case 'TelaA':
+                iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+                break;
+            case 'TelaB':
+                iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+                break;
+            case 'TelaC':
+                iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+                break;
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+    })}
+    tabBarOptions={{
         activeTintColor: 'red',
         inactiveTintColor: 'blue',
-        labelStyle: { fontSize: 30 },
     }} initialRouteName="TelaB">
-        <Tab.Screen name="TelaA" component={TelaA} />
-        <Tab.Screen name="TelaB" component={TelaB} />
-        <Tab.Screen name="TelaC" component={TelaC} />
+        <Tab.Screen name="TelaA" component={TelaA} options={{title: 'Tela A'}} />
+        <Tab.Screen name="TelaB" component={TelaB} options={{title: 'Tela B'}} />
+        <Tab.Screen name="TelaC" component={TelaC} options={{title: 'Tela C'}} />
     </Tab.Navigator>
 );
