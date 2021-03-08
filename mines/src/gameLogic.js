ff330069570ec8f1488e362bbee391628ee9d568
Victor Hugo Batista <vct.bta@gmail.com>
2021-03-08 12:17:17 -0300

    Adiciona o restante da lógica do campo minado

--- mines/src/gameLogic.js
+++ mines/src/gameLogic.js
@@ -35,8 +35,74 @@ const spreadMines = (board, minesAmount) => {
     }
 };
 
-export const createMinedBoard = (rows, cols, minesAmount) => {
+const createMinedBoard = (rows, cols, minesAmount) => {
     const board = createBoard(rows, cols);
     spreadMines(board, minesAmount);
     return board;
+};
+
+const cloneBoard = board => {
+    board.map(rows => {
+        return rows.map(field => ({... field}));
+    });
+};
+
+const getNeighbors = (board, row, col) => {
+    const neighbors = [];
+    const rows = [row - 1, row, row + 1];
+    const cols = [col - 1, col, col + 1];
+    rows.forEach(r => {
+        cols.forEach(c => {
+            const isCurrent = r === row && c === col;
+            const isValidRow = r >= 0 && r < board.length;
+            const isValidCol = c >= 0 && c < board[0].length;
+            if (isCurrent || ! isValidRow || ! isValidCol) {
+                return;
+            }
+            neighbors.push(board[r][c]);
+        });
+    });
+};
+
+const safeNeighborhood = (board, row, col) => {
+    const safes = (result, neighbor) => result && ! neighbor.mined;
+    return getNeighbors(board, row, col).reduce(safes, true);
+};
+
+const openField = (board, row, col) => {
+    const field = board[row][col];
+    if (! field.opened) {
+        field.opened = true;
+        if (field.mined) {
+            field.exploded = true;
+        } else if (safeNeighborhood(board, row, col)) {
+            getNeighbors(board, row, col)
+                .forEach(n => openField(board, n.row, n.row));
+        } else {
+            const neighbors = getNeighbors(board, row, col);
+            field.nearMines = neighbors.filter(n => n.mined).length;
+        }
+    }
+};
+
+const fields = board => [].concat(...board);
+
+const hasExplosion = board => !! fields(board)
+    .filter(f => f.exploded).length;
+
+const pending = field => (field.mined && ! field.flagged) ||
+    (! field.mined && ! field.opened);
+
+const wonGame = board => ! field(board).filter(pending).length;
+
+const showMines = board => field(board).filter(field => field.mined)
+    .forEach(field => field.opened = true);
+
+export {
+    createMinedBoard,
+    cloneBoard,
+    openField,
+    hasExplosion,
+    wonGame,
+    showMines,
 };
